c97c295171e7835ddf7e5727a2dbfe131bcd94c0
PranayKumar31399 <janprana@publicisgroupe.net>
2024-07-12 15:43:17 +0530

    tailwind poc

--- host/src/App.tsx
+++ host/src/App.tsx
@@ -1,16 +1,16 @@
 import React from "react";
+import "./index.css";
+import "tailwindcss/tailwind.css";
 
 const RemoteApp = React.lazy(() => import("remoteApp/App"));
 
 const App: React.FC = () => {
-
   return (
     <div>
       <React.Suspense fallback={null}>
-        <RemoteApp basepath="/remoteApp"/>
+        <RemoteApp basepath="/remoteApp" />
       </React.Suspense>
     </div>
-    
   );
 };
 
--- remote/src/App.tsx
+++ remote/src/App.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { Routes, Route, BrowserRouter} from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import routes from "./routes/routes";
 import Sidebar from "./components/Sidebar";
+import "./index.css";
+import "tailwindcss/tailwind.css";
 
 // Define the shape of a route
 interface RouteType {
@@ -16,18 +18,35 @@ interface AppProps {
 
 const App: React.FC<AppProps> = ({ basepath = "" }) => {
   return (
-    <div>
-    <BrowserRouter>
-      <Sidebar basepath={basepath} />
-      <Routes>
-        {routes.map((route: RouteType, index: number) => (
-          <Route key={index} path={basepath + route.path} element={route.element} />
-        ))}
-      </Routes>
-    </BrowserRouter>
-
+    <div className="min-h-screen flex">
+      <BrowserRouter>
+        <div className="flex flex-row w-full">
+          <div className="w-1/5 bg-gray-800 text-white p-4">
+            <Sidebar basepath={basepath} />
+          </div>
+          <div className="w-4/5 p-4">
+            <Routes>
+              {routes.map((route: RouteType, index: number) => (
+                <Route
+                  key={index}
+                  path={basepath + route.path}
+                  element={route.element}
+                />
+              ))}
+            </Routes>
+            <div>
+              <div className="mt-4 p-4 bg-green-400 shadow-md">
+                This is in remote app with in-built tailwindcss utilites.
+              </div>
+              <div className="mt-4 p-4 bg-header-logo-secondary shadow-md">
+                This is in remote app with custom tailwindcss utilites from
+                host.
+              </div>
+            </div>
+          </div>
+        </div>
+      </BrowserRouter>
     </div>
-    
   );
 };
 
